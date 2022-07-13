ba3633e3e437844532bf6c97ba0eedb81edc7ddc
joaovaladao <joaovaladao161274@gmail.com>
2022-07-13 15:47:35 -0300

    Triangulação para cálculo do valor de acesso noturno
    Co-authored-by: italovinicius18 <italovinicius18@yahoo.com>
    Co-authored-by: IanFPFerreira <ianfillipe@gmail.com>

--- _test/Estacionamento_test.py
+++ _test/Estacionamento_test.py
@@ -7,6 +7,8 @@ estacionamento_1 = Estacionamento('Estacionamento 1', 100, 0, 30, 102.0, 120.0, 
 
 estacionamento_2 = Estacionamento('Estacionamento 2', 200, 0, 20, 72.0, 70.0, 21.0, 1.0, 1.0, '08:00', '18:00', '00:00', '23:59', 'R$ 0,00')
 
+estacionamento_3 = Estacionamento('Estacionamento 3', 400, 0, 10, 40.0, 50.0, 20.0, 1.0, 1.0, '08:00', '18:00', '00:00', '23:59', 'R$ 0,00')
+
 #------------------------------------ Horas Cheias ------------------------------------
 
 @pytest.mark.TesteFuncional
@@ -33,5 +35,10 @@ def teste_1_acesso_noturno():
 
 @pytest.mark.TesteFuncional
 def teste_2_acesso_noturno():
-    acesso = Acesso('JF42A', '02:00', '03:30', 'acesso noturno', '20.0', '0.6')
+    acesso = Acesso('JF42A', '11:00', '03:30', 'acesso noturno', '20.0', '0.6')
     assert estacionamento_2.calcula_acesso_noturno(acesso) == 21.0
+
+@pytest.mark.TesteFuncional
+def teste_3_acesso_noturno():
+    acesso = Acesso('AB64A', '22:00', '07:59', 'acesso noturno', '10.0', '0.7')
+    assert estacionamento_3.calcula_acesso_noturno(acesso) == 20.0
